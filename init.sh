46cd620c53d24ea68c4cf41e8da2285dd008a813
Alan Bernstein <a.bernstein@sprinteins.com>
2022-01-02 13:00:56 +0100

    add synapse helm chart

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # start helm
-helm uninstall element dendrite > /dev/null
-helm install dendrite "./dendrite/helm/"
+helm uninstall element synapse > /dev/null
+helm install synapse "./synapse/helm/"
 helm install element "./element/helm/"
 
 # start k8s dashboard
 pkill -9 -f "kubectl proxy"
-kubectl proxy
+kubectl proxy &
